b830464fa691b1a4b38c58ec186f30f03d0a095b
Himath Ranaweera <145730950+HimathRanaweera@users.noreply.github.com>
2024-03-26 08:44:59 +0530

    Create About.jsx file

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -26,10 +26,10 @@ function Header() {
                 <div class="container d-flex flex-wrap justify-content-center">
                     <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none">
                         <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap" /></svg>
-                        <span class="fs-4">Double header</span>
+                        <span class="fs-4">Clothify Story</span>
                     </a>
                     <form class="col-12 col-lg-auto mb-3 mb-lg-0" role="search">
-                        <input type="search" class="form-control" placeholder="Search..." aria-label="Search" />
+                        <input type="search" class="form-control" placeholder="Search" aria-label="Search" />
                     </form>
                 </div>
             </header>
